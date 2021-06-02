a6cedd62cb653848cc71ee1c3f33b04047104e26
Elin Amren <elin.amren@gmail.com>
2021-06-02 21:40:27 +0200

    changed camera in end

--- script.js
+++ script.js
@@ -1,62 +1,50 @@
 const marysHouse = document.querySelector(".house");
 const input = document.querySelector("#password");
-const patternOne = document.querySelector(".pattern-one");
-const patternTwo = document.querySelector(".pattern-two");
-const patternThree = document.querySelector(".pattern-three");
-const patternFour = document.querySelector(".pattern-four");
 const message = document.querySelector("#message");
-
-let challengesCompleted = 0;
+const pattern = document.querySelector(".pattern");
+const patternImage = document.querySelector("#pattern-image");
 
 const firstChallengeHandler = () => {
   window.location.href = "maryshouse.html";
-  challengesCompleted++;
-  console.log(challengesCompleted);
 };
 
-const closeAllPatterns = () => {
-  patternTwo.classList.add("hidden");
-  patternThree.classList.add("hidden");
-  patternOne.classList.add("hidden");
-  patternFour.classList.add("hidden");
+const closePattern = () => {
+  pattern.classList.add("hidden");
 };
-const getPatternOne = () => {
-  patternOne.classList.toggle("hidden");
-  patternTwo.classList.add("hidden");
-  patternThree.classList.add("hidden");
-  patternFour.classList.add("hidden");
+
+const one = "media/pattern-mouth.png";
+const two = "media/pattern-hand.png";
+const three = "media/pattern-grasshopper.png";
+const four = "media/pattern-pleasantville.png";
+
+const getPatternOne = (one) => {
+  pattern.classList.remove("hidden");
+  patternImage.src = one;
 };
 const getPatternTwo = () => {
-  patternTwo.classList.toggle("hidden");
-  patternOne.classList.add("hidden");
-  patternThree.classList.add("hidden");
-  patternFour.classList.add("hidden");
+  pattern.classList.remove("hidden");
+  patternImage.src = two;
 };
 const getPatternThree = () => {
-  patternThree.classList.toggle("hidden");
-  patternTwo.classList.add("hidden");
-  patternOne.classList.add("hidden");
-  patternFour.classList.add("hidden");
+  pattern.classList.remove("hidden");
+  patternImage.src = three;
 };
 const getPatternFour = () => {
-  patternFour.classList.toggle("hidden");
-  patternTwo.classList.add("hidden");
-  patternThree.classList.add("hidden");
-  patternOne.classList.add("hidden");
+  pattern.classList.remove("hidden");
+  patternImage.src = four;
 };
 
 const showHouse = () => {
   console.log(input.value);
-  if (input.value === "rose") {
-    closeAllPatterns();
+  if (input.value.toLowerCase() === "rose") {
+    closePattern();
     marysHouse.style.display = "inline-block";
     message.innerText = "You are ready! Go to her house, before its to late.";
-  } else if (input.value.includes("r")) {
-    closeAllPatterns();
-
+  } else if (input.value.toLowerCase().includes("r")) {
+    closePattern();
     message.innerText = "You are on the right way!";
   } else {
-    closeAllPatterns();
+    closePattern();
 
     message.innerText = "You are not ready to meet Mary just yet..";
     marysHouse.style.display = "none";
@@ -72,9 +60,9 @@ const speechbubble = document.querySelector(".speechbubble");
 
 conversations = [
   "I am looking for someone. <br>Have you noticed anything?",
-  "Mary: I might have heard something about someone, let me think. <br>I am about to make an apple pie, can you get some apples for me? <br>5 would be enough.",
-  "Mary: Thank you dear. I remember I met someone, someone who tried to figure out a riddle. It was long ago but it's still stuck in my mind, maybe you know the right answer. I don't have wings, but I can fly. I don't have eyes, but I will cry!What am I? Look for me!",
-  "Mary: I don’t know anything about that. It seems that someone is pointing to the medium, she'll give you all the answers you seek. Take the shortcut through the corn field.",
+  "<i>Mary:</i> I might have heard something about someone, let me think. <br><br>I am about to make an apple pie, can you get some apples for me? <br>5 would be enough.",
+  "<i>Mary:</i> Thank you dear. I remember I met someone, someone who tried to figure out a riddle. It was long ago but it's still stuck in my mind, maybe you know the right answer. I don't have wings, but I can fly. I don't have eyes, but I will cry!What am I? Look for me!",
+  "<i>Mary:</i> I don’t know anything about that. It seems that someone is pointing to the medium, she'll give you all the answers you seek. Take the shortcut through the corn field.",
 ];
 let times = 0;
 
